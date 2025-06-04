12b52403a0ca74cfac646f95973adbf88a9b760b
AtsuyaYoshimoto <155902565+AtsuyaYoshimoto@users.noreply.github.com>
2025-06-04 14:25:17 +0900

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -805,6 +805,9 @@ function displayAIPredictionResult(aiResult) {
         if (document.getElementById('recommended-exacta')) {
             document.getElementById('recommended-exacta').textContent = recs.exacta.combination.join('-');
         }
+        if (document.getElementById('recommended-trio')) {
+        document.getElementById('recommended-trio').textContent = recs.trio.combination.join('-');
+        }
     }
     
     // alert削除
